2339b5fa2177fc70d249c1fde4729dff7c7a7bd2
01021029 <01021029@M1538.acad.unifeso.lan>
2018-11-21 21:00:32 -0200

    Add um for no código

--- Mark1/src/br/com/guix1998/get/HelloGet.java
+++ Mark1/src/br/com/guix1998/get/HelloGet.java
@@ -4,17 +4,15 @@
 package br.com.guix1998.get;
 
 /**
- * @author 01021029
  *Exemplo de Integração
  */
 public class HelloGet {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		System.out.println("Eu sou Batman");
-
+		for(int i=3; i>0; i--){
+			System.out.println(i);
+		}
 	}
 
 }
