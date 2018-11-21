89f24dcc3f97e4c0ac59e10a68dbd9b2b7e40bd7
guix1998 <45245382+guix1998@users.noreply.github.com>
2018-11-21 20:52:20 -0200

    Add Arquivos via upload

---
+++ Mark1/src/br/com/guix1998/get/HelloGet.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.guix1998.get;
+
+/**
+ * @author 01021029
+ *Exemplo de Integração
+ */
+public class HelloGet {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Eu sou Batman");
+
+	}
+
+}
